2bca02d044b1ea979218532bea5cfb984afda273
Siarhei Prymakovich <prima.psa@gmail.com>
2023-02-28 10:14:53 +0300

    feat: implement FixStringCase

--- FixStringCase.js
+++ FixStringCase.js
@@ -3,3 +3,4 @@ const solve = (s) => {
     const upperCount = [...s].reduce((acc,e) => e === e.toUpperCase() ? acc + 1 : acc, 0);
     return upperCount > s.length / 2 ? s.toUpperCase() : s.toLowerCase();
 }
+
