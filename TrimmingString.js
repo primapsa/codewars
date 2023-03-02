4b044cf9e5e42ae974391a52a5d7517bd521e0a0
Siarhei Prymakovich <prima.psa@gmail.com>
2023-03-02 10:23:22 +0300

    feat: implement trim

--- TrimmingString.js
+++ TrimmingString.js
@@ -1,7 +1,7 @@
 
 const trim = (str, size) =>
 {
-    const cut = str.length <= 3 ? size : size < 4 ? size : size -3
+    const cut = str.length <= 3 ? size : size <= 3 ? size : size -3
     const points = str.length < size ? '' : '...';
     return str.length <= size ? str : str.slice(0, cut) + points;
 }
