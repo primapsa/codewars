18c9d9d32c1262cca98f42ce4305051d630250ff
Sergey Primakovich <prima.psa@gmail.com>
2024-04-12 11:10:09 +0300

    feat: implement findOutlier

--- Find The Parity Outlier.js
+++ Find The Parity Outlier.js
@@ -1,6 +1,6 @@
 const findOutlier = (integers) => {
     const copy = [...integers]
     const oddCount = copy.slice(0,3).filter( num => num % 2 === 0).length;
-    return copy.find(num => oddCount > 1 ? num % 2 !== 0 : num % 2 === 0  )
+    return copy.find(num => oddCount > 1 ? num % 2 !== 0 : num % 2 === 0 )
 }
 console.log(findOutlier([0,1,2]))
